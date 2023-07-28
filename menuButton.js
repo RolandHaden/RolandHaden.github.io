7061b219be99fbf890fc62e440fd2a0bb3f80564
RolandHaden <davis.haden@gmail.com>
2023-07-28 12:53:36 -0400

    first

---
+++ menuButton.js
@@ -0,0 +1,18 @@
+const myObject = document.getElementById("myObject");
+const ringClose = document.querySelector(".ringClose");
+
+
+myObject.addEventListener("click", () => {
+    myObject.classList.toggle("active");
+    if (myObject.classList.contains("active")) {
+        ringClose.style.visibility = "visible";
+      } else {
+        ringClose.style.visibility = "hidden";
+      }
+  });
+
+myObject.addEventListener("blur", () => {
+  myObject.classList.remove("active");
+  ringClose.style.visibility = "hidden";
+});
+
